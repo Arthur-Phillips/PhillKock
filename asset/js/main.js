69ab010b30e81d077398332c03732ed3038bedbd
Arthur Phillips <arthur-phillips@protonmail.com>
2019-09-07 01:05:10 +0200

    Image Hero + Overlay Beginning + Images
    Added a hero image in the background on the home page <Currently random>
    + The full logo over
    
    Added the start of an overlay and connected it to the navigation arrows in the navbar on the home page

---
+++ asset/js/main.js
@@ -0,0 +1,11 @@
+console.log("Test"); //Test if linked
+
+//Overlay
+function overlayOn() {
+    document.getElementById("overlay").style.display = "block";
+}
+
+function overlayOff() {
+    document.getElementById("overlay").style.display = "none";
+}
+//end
