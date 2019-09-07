af2cb4fe24c697e3450959e2cf21e5a6e33e6104
Arthur Phillips <arthur-phillips@protonmail.com>
2019-09-07 17:44:58 +0200

    Navigation Arrows Change
    The navigation arrows on the sidebar change according to the overlay being displayed or not

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,4 +1,5 @@
 console.log("Test"); //Test if linked
+var doubleArrows = 1; //Used as a global var for switching double arrows
 
 //Overlay
 function overlayOn() {
@@ -7,5 +8,19 @@ function overlayOn() {
 
 function overlayOff() {
     document.getElementById("overlay").style.display = "none";
+}
+//end
+
+//Overlay Arrows
+function navigationArrows() {
+    if (doubleArrows === 1) {
+        doubleArrows = 0;
+        document.getElementById("navigation-arrow__right").style.display = "none";
+        document.getElementById("navigation-arrow__left").style.display = "block";
+    } else if (doubleArrows === 0) {
+        doubleArrows = 1;
+        document.getElementById("navigation-arrow__right").style.display = "block";
+        document.getElementById("navigation-arrow__left").style.display = "none";
+    }
 }
 //end
